63b441af3ed81986db6ac265c32cda42b097f8e7
TahaMajlesi <186780445+TahaMjp@users.noreply.github.com>
2025-05-07 06:06:42 -0700

    update

--- src/Components/Filtersplace/Filtersplace.js
+++ src/Components/Filtersplace/Filtersplace.js
@@ -1,5 +1,7 @@
 const Filtersplace = () => {
-  return <div className="w-3/12 flex bg-green-300"> s </div>;
+  return (
+    <div className="w-2/12 flex bg-green-300 h-10/12 fixed right-20 border-2 border-black font-semibold tracking-widest p-2"></div>
+  );
 };
 
 export default Filtersplace;
--- src/Components/Imageplace/Imageplace.js
+++ src/Components/Imageplace/Imageplace.js
@@ -2,7 +2,7 @@ import Getimage from "../Getimage/Getimage";
 
 const Imageplace = () => {
   return (
-    <div className="w-8/12 columns-3 gap-3">
+    <div className="w-9/12 columns-3 gap-3">
       <Getimage />
     </div>
   );
